f9192286190d840ba59935695668faca20171f66
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-05-04 19:07:11 +0300

    Rework all code Add support for '1-9' and 'Backspace' buttons Add comments

--- script.js
+++ script.js
@@ -1,110 +1,109 @@
+'use strict';
 const bodyElement = document.body;
 const screenCurrent = document.querySelector('.screen__current  ');
 const screenPrevious = document.querySelector('.screen__previous');
-const btnsNumbers = document.querySelectorAll('.buttons__number');
-const btnsOperations = document.querySelectorAll('.buttons__operation');
 const btnClean = document.getElementById('clean');
-const bntDelete = document.getElementById('delete');
-const btnPercent = document.getElementById('percent');
-const bntDivide = document.getElementById('divide');
-const btnOne = document.getElementById('one');
-const btnTwo = document.getElementById('two');
-const btnThree = document.getElementById('three');
-const btnMultiply = document.getElementById('multiply');
-const btnFour = document.getElementById('four');
-const btnFive = document.getElementById('five');
-const btnSix = document.getElementById('six');
-const btnSum = document.getElementById('sum');
-const btnSeven = document.getElementById('seven');
-const btnEight = document.getElementById('eight');
-const btnNine = document.getElementById('nine');
-const btnSubstract = document.getElementById('minus');
-const btnDot = document.getElementById('dot');
-const btnZero = document.getElementById('zero');
+const btnDelete = document.getElementById('delete');
 const btnEqual = document.getElementById('equal');
-
-//
-// Functionality of number buttons.
-//
-
-bodyElement.addEventListener('keydown', (event) => {
-  console.log(event.key);
-  if (isFinite(event.key)) {
-    screenCurrent.textContent += event.key;
-  } else if (event.key == 'Backspace') {
-    screenCurrent.textContent = [...screenCurrent.textContent].slice(0, this.length - 1).join('');
-  }
-});
-
-bodyElement.addEventListener('click', (event) => {
-  if (Number(event.target.textContent) || event.target.textContent == '.')
-    screenCurrent.textContent += event.target.textContent;
-});
-
-//
-// Operation functionality
-//
+const btnsOperations = document.querySelectorAll('.math');
+const operatEl = document.querySelectorAll('.buttons__number, .math, .buttons__operation__clean');
 
 let operator, numbers, result;
-bodyElement.addEventListener('click', (event) => {
-  switch (event.target) {
-    case btnClean:
-      screenCurrent.textContent = '';
-      screenPrevious.textContent = '';
-      break;
-    case bntDelete:
-      screenCurrent.textContent = [...screenCurrent.textContent].slice(0, this.length - 1).join('');
-      break;
-    case btnPercent:
-      screenCurrent.textContent += '%';
-      operator = '%';
-      break;
-    case bntDivide:
-      screenCurrent.textContent += '÷';
-      operator = '/';
-      break;
-    case btnMultiply:
-      screenCurrent.textContent += '*';
-      operator = '*';
-      break;
-    case btnSum:
-      screenCurrent.textContent += '+';
-      operator = '+';
-      break;
-    case btnSubstract:
-      screenCurrent.textContent += '-';
-      operator = '-';
-      break;
+operator = '';
 
-    case btnEqual:
-      // numbers = screenCurrent.textContent.split(/(?!^)[(\W)?]/);
-      numbers = screenCurrent.textContent.includes(/[]/);
+//
+//  Functions to calculate result
+//
+const calc = function () {
+  // Using key : value for all operations
+  const operations = {
+    '%': function (a, b) {
+      return a * (0.01 * b);
+    },
+    '+': function (a, b) {
+      return a + b;
+    },
+    '-': function (a, b) {
+      return a - b;
+    },
+    '*': function (a, b) {
+      return a * b;
+    },
+    '/': function (a, b) {
+      return a / b;
+    },
+  };
+  // Using saved operatorion sign from event listener and get math calculation that we need, but before that we convert string into numbers.
+  result = operations[operator](Number(numbers[0]), Number(numbers[1]));
+  if (!Number.isInteger(result)) result = result.toFixed(2);
+  // Updating screen result
+  screenCurrent.textContent = result;
+  // Reset
+  operator = '';
+};
 
-      switch (operator) {
-        case '%':
-          result = Number(numbers[0]) * (0.01 * Number(numbers[1]));
-          break;
-        case '/':
-          result = Number(numbers[0]) / Number(numbers[1]);
-          break;
-        case '*':
-          result = Number(numbers[0]) * Number(numbers[1]);
-          break;
-        case '+':
-          result = Number(numbers[0]) + Number(numbers[1]);
-          break;
-        case '-':
-          result = Number(numbers[0]) - Number(numbers[1]);
-          break;
+//
+// Code for typing numbers with keyboard
+bodyElement.addEventListener('keydown', (event) => {
+  if (isFinite(event.key)) {
+    screenCurrent.textContent += event.key;
+  }
+  // Add backspace keyboard button as functionality
+  else if (event.key == 'Backspace') {
+    screenCurrent.textContent = [...screenCurrent.textContent].slice(0, this.length - 1).join('');
+  }
+  //Add this line to update numbers array when someone using keyobard for that
+  numbers = screenCurrent.textContent.split(/[%\/*+-]/);
+});
 
-        default:
-          console.log('nope');
-          break;
+//
+// Code for updating numbers, add operations and clear/delete.
+operatEl.forEach((item) =>
+  item.addEventListener('click', (event) => {
+    // Checking if we need to get reesult when typing math operation button or equal button
+    if (
+      Array.from(btnsOperations).includes(event.target) &&
+      operator !== '' &&
+      !numbers.includes('') &&
+      numbers.length === 2
+    ) {
+      numbers = screenCurrent.textContent.split(/[%\/*+-]/);
+      screenPrevious.textContent = screenCurrent.textContent;
+
+      calc();
+    } else {
+      // If for updating screen with numbers that person typing
+      if (Number(event.target.textContent) || event.target.textContent === '.' || event.target.textContent === '0') {
+        screenCurrent.textContent += event.target.textContent;
       }
 
-      if (!Number.isInteger(result)) result = result.toFixed(2);
-      screenCurrent.textContent = result;
-  }
-  // btnsNumbers.forEach((item) => (item.disabled = false));
-  // btnsOperations.forEach((item) => (item.disabled = true));
-});
+      // Delete button functionality
+      else if (event.target == btnDelete) {
+        screenCurrent.textContent = [...screenCurrent.textContent].slice(0, this.length - 1).join('');
+      }
+
+      // Clear button functionality
+      else if (event.target === btnClean) {
+        operator = '';
+        screenCurrent.textContent = '';
+        screenPrevious.textContent = '';
+      }
+
+      // Code to update operator and change it if person deside to use another
+      // Only if screen is not empty, and user click on operation button
+      else if (
+        Number(event.target.textContent) !== NaN &&
+        event.target.textContent !== '0' &&
+        screenCurrent.textContent
+      ) {
+        // if (operator !== '' && /[+\-*\/%]/.test(screenCurrent.textContent)) {
+        //   operator = '';
+        //   screenCurrent.textContent = [...screenCurrent.textContent].slice(0, this.length - 1).join('');
+        // }
+        screenCurrent.textContent += event.target.textContent;
+        operator = event.target.textContent;
+      }
+      numbers = screenCurrent.textContent.split(/[%\/*+-]/);
+    }
+  })
+);
